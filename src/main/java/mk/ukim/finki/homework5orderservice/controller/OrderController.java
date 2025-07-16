2824f4246991c1606bd4e610c833b0a308087c5c
MilanTodorovikj <milan.todorovik01@gmail.com>
2025-07-16 20:15:05 +0200

    test dev workflow

--- src/main/java/mk/ukim/finki/homework5orderservice/controller/OrderController.java
+++ src/main/java/mk/ukim/finki/homework5orderservice/controller/OrderController.java
@@ -18,7 +18,8 @@ public class OrderController {
     private final List<Order> orders = List.of(
             new Order(1L, "Laptop", 1L),
             new Order(2L, "Phone", 2L),
-            new Order(3L, "PC", 1L)
+            new Order(3L, "PC", 1L),
+            new Order(4L, "TV", 2L)
     );
 
     @GetMapping("/{id}")
